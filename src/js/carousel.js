8c971f53439534557883e300c1a5c4295bd952b7
viarmoly <v.iarmolitskyi@gmail.com>
2019-05-05 03:21:48 +0300

    Changing header and slider

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -1,46 +1,52 @@
 var slideIndex = 0;
 
-
 carousel();
 
 function carousel() {
-   var i;
+    var i;
     var x = document.getElementsByClassName("mySlides");
     for (i = 0; i < x.length; i++) {
         x[i].style.display = "none";
     }
     slideIndex++;
-    if (slideIndex > x.length) {slideIndex = 1}
+    if (slideIndex > x.length) {
+        slideIndex = 1
+    }
 
-    x[slideIndex-1].style.display = "block";
-    setTimeout(carousel, 150000); // Change image every 2 seconds
+    x[slideIndex - 1].style.display = "block";
+    setTimeout(carousel, 10000); // Change image every 2 seconds
 }
+
 showSlides(slideIndex);
 
 function showSlides(n) {
     var i;
     var slides = document.getElementsByClassName("mySlides");
     var dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
+    if (n > slides.length) {
+        slideIndex = 1
+    }
+    if (n < 1) {
+        slideIndex = slides.length
+    }
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
-    slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
 }
 
 Array.from(document.querySelectorAll('a.next')).forEach(cls => {
-    cls.addEventListener('click', function() {
+    cls.addEventListener('click', function () {
         showSlides(slideIndex += 1);
     });
 });
 
 Array.from(document.querySelectorAll('a.prev')).forEach(cls => {
-    cls.addEventListener('click', function() {
+    cls.addEventListener('click', function () {
         showSlides(slideIndex += -1);
     });
 });
